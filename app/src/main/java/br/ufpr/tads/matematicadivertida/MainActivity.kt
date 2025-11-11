ce6dd0efda0f1edd46063ae58c5b96a87b2ee1f6
Adriano <adriano.lols@gmail.com>
2025-11-11 19:05:13 -0300

    Flesh out MainActivity menu

--- app/src/main/java/br/ufpr/tads/matematicadivertida/MainActivity.kt
+++ app/src/main/java/br/ufpr/tads/matematicadivertida/MainActivity.kt
@@ -1,12 +1,18 @@
 package br.ufpr.tads.matematicadivertida
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button1 : Button
+    private lateinit var button2 : Button
+    private lateinit var button3 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        button1 = findViewById<Button>(R.id.button1)
+        button2 = findViewById<Button>(R.id.button2)
+        button3 = findViewById<Button>(R.id.button3)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, ContagemActivity::class.java)
+            startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent(this, AritimeticaActivity::class.java)
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            val intent = Intent(this, MaiorActivity::class.java)
+            startActivity(intent)
         }
     }
 }
